b15852640b19ae8b0f4a71a0dc2cce7760956db1
SSripilaipong <santhapon.s@siametrics.com>
2022-02-18 13:51:44 +0700

    test: Use MockBean of UserService for unit test of UserControllerTest
    Use MockBean instead of real UserService to test the contact between Controller and Service
    
    Refs: #1

--- week1/src/test/java/com/example/week1/unit/UserControllerTest.java
+++ week1/src/test/java/com/example/week1/unit/UserControllerTest.java
@@ -1,10 +1,13 @@
 package com.example.week1.unit;
 
 import com.example.week1.TestRequester;
+import com.example.week1.user.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
@@ -12,10 +15,15 @@ public class UserControllerTest {
     @Autowired
     private TestRequester requester;
 
+    @MockBean
+    private UserService userService;
+
     @Test
     public void shouldReturnTokenWhenLoginWithUsernameAndPassword() {
-        LoginRequest loginRequest = new LoginRequest("Santhapon", "Admin1234");
+        Mockito.when(userService.login("MyUsername", "MyPassword")).thenReturn("MyToken");
+
+        LoginRequest loginRequest = new LoginRequest("MyUsername", "MyPassword");
         LoginSuccessResponse response = requester.post("/login", loginRequest, LoginSuccessResponse.class);
-        Assertions.assertNotEquals(null, response.getToken());
+        Assertions.assertEquals("MyToken", response.getToken());
     }
 }
