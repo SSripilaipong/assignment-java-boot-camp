bc7127f2f8b9ece4f36395934a1e33bd2f63b8e9
SSripilaipong <santhapon.s@siametrics.com>
2022-02-19 00:31:34 +0700

    feat: implement code for unit test shouldReturnProductsWithKeywordInNames in ProductControllerTest
    implement ProductController.searchProductByKeywords()
    
    Refs: #2

---
+++ week1/src/main/java/com/example/week1/product/ProductController.java
@@ -0,0 +1,19 @@
+package com.example.week1.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    @ResponseBody
+    public Products searchProductByKeywords(@RequestParam String keyword) {
+        return productService.searchProducts(keyword);
+    }
+}
--- week1/src/main/java/com/example/week1/product/Products.java
+++ week1/src/main/java/com/example/week1/product/Products.java
@@ -1,21 +1,35 @@
 package com.example.week1.product;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class Products implements Iterable<Product> {
+    private final List<Product> products;
+
+    public Products() {
+        products = new ArrayList<>();
+    }
+
     @Override
     public Iterator<Product> iterator() {
         return new ProductsIterator(this);
     }
 
     public int size() {
-        return 0;
+        return products.size();
     }
 
     public void add(Product product) {
+        products.add(product);
     }
 
     public Product get(int index) {
-        return new Product();
+        return products.get(index);
+    }
+
+    // for JSON serialization
+    public List<Product> getProducts() {
+        return products;
     }
 }
