3af25c9fcb422a0592c84d87d82fabdc04d02856
SSripilaipong <santhapon.s@siametrics.com>
2022-02-18 14:00:18 +0700

    refactor: change return type of TestRequester to ResponseEntity<T>
    Refs: #1

--- week1/src/test/java/com/example/week1/TestRequester.java
+++ week1/src/test/java/com/example/week1/TestRequester.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,11 +14,11 @@ public class TestRequester {
     @Autowired
     private TestRestTemplate rest;
 
-    public <T, R extends JsonConvertible> T post(String url, R requestObject, Class<T> responseClass) {
+    public <T, R extends JsonConvertible> ResponseEntity<T> post(String url, R requestObject, Class<T> responseClass) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request = new HttpEntity<>(requestObject.toJsonString(), headers);
 
-        return rest.postForObject(url, request, responseClass);
+        return rest.postForEntity(url, request, responseClass);
     }
 }
--- week1/src/test/java/com/example/week1/unit/UserControllerTest.java
+++ week1/src/test/java/com/example/week1/unit/UserControllerTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
@@ -23,7 +24,10 @@ public class UserControllerTest {
         Mockito.when(userService.login("MyUsername", "MyPassword")).thenReturn("MyToken");
 
         LoginRequest loginRequest = new LoginRequest("MyUsername", "MyPassword");
-        LoginSuccessResponse response = requester.post("/login", loginRequest, LoginSuccessResponse.class);
+        LoginSuccessResponse response =
+                requester.post("/login", loginRequest, LoginSuccessResponse.class).getBody();
+
+        assert response != null;
         Assertions.assertEquals("MyToken", response.getToken());
     }
 }
