457a2b4f45901a09a331f6404d4705854f8f58d0
SSripilaipong <santhapon.s@siametrics.com>
2022-02-19 00:52:42 +0700

    feat: implement code for shouldReturnProductsWithKeywordInNames in ProductServiceTest
    implement ProductService.searchProducts()
    
    Refs: #2

--- week1/src/main/java/com/example/week1/product/ProductService.java
+++ week1/src/main/java/com/example/week1/product/ProductService.java
@@ -3,13 +3,20 @@ package com.example.week1.product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
     public Products searchProducts(String keyword) {
-        return new Products();
+        List<Product> productList = productRepository.findByNameContaining(keyword);
+        Products result = new Products();
+        for(Product product: productList) {
+            result.add(product);
+        }
+        return result;
     }
 
     public void setProductRepository(ProductRepository productRepository) {
