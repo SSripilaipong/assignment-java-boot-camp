443c12e20f57d40b04fc3ba98ec5871ff02c9a61
SSripilaipong <santhapon.s@siametrics.com>
2022-02-19 01:12:45 +0700

    feat: create initial products
    create initial products for acceptance test
    
    Refs: #2

--- week1/src/main/java/com/example/week1/Week1Application.java
+++ week1/src/main/java/com/example/week1/Week1Application.java
@@ -1,5 +1,7 @@
 package com.example.week1;
 
+import com.example.week1.product.Product;
+import com.example.week1.product.ProductService;
 import com.example.week1.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -13,9 +15,21 @@ public class Week1Application {
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private ProductService productService;
+
 	@PostConstruct
 	public void createInitialUser() {
 		userService.register("Santhapon", "Admin1234");
+	}
+
+	@PostConstruct
+	public void createInitialProducts() {
+		productService.addNewProduct(new Product(0, "Adidas NMD R1 Pimeknit Core Black"));
+		productService.addNewProduct(new Product(1, "Adidas NMD R1 PK Japan Triple Black"));
+		productService.addNewProduct(new Product(2, "POCA SHOE NMD Sneakers Fashion"));
+		productService.addNewProduct(new Product(3, "Something Irrelevant"));
+		productService.addNewProduct(new Product(4, "Adidas NMD R1 Color Core Black"));
 	}
 
 	public static void main(String[] args) {
