2eed4c550b35223f7caa1af53ac6a71583f1f6ec
SSripilaipong <santhapon.s@siametrics.com>
2022-02-18 18:33:01 +0700

    feat: implement code for unit test shouldRegisterNewUser in UserServiceTest
    implement UserService.register()
    
    Refs: #1

--- week1/src/main/java/com/example/week1/user/UserService.java
+++ week1/src/main/java/com/example/week1/user/UserService.java
@@ -1,12 +1,20 @@
 package com.example.week1.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     public String login(String username, String password) {
         return username;
+    }
+
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public String getUsernameFromToken(String token) {
@@ -14,5 +22,7 @@ public class UserService {
     }
 
     public void register(String username, String password) {
+        User user = new User(username, password);
+        userRepository.save(user);
     }
 }
