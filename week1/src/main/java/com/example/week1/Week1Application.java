2d0a634014c2360843152e06cea1f32ffa485668
SSripilaipong <santhapon.s@siametrics.com>
2022-02-18 19:22:46 +0700

    feat: create initial user
    create initial user to complete the acceptance test
    username: "Santhapon"
    password: "Admin1234"
    
    Refs: #1

--- week1/src/main/java/com/example/week1/Week1Application.java
+++ week1/src/main/java/com/example/week1/Week1Application.java
@@ -1,10 +1,22 @@
 package com.example.week1;
 
+import com.example.week1.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class Week1Application {
+
+	@Autowired
+	private UserService userService;
+
+	@PostConstruct
+	public void createInitialUser() {
+		userService.register("Santhapon", "Admin1234");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Week1Application.class, args);
