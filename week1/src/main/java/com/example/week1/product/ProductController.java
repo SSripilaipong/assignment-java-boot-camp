39e4c76ef9b7b887b6c56658cff34fbb8aca4aef
SSripilaipong <santhapon.s@siametrics.com>
2022-02-19 21:24:56 +0700

    feat: implement ProductController.getProductDetailById() for unit test shouldFindProductWithId
    Refs: #3

--- week1/src/main/java/com/example/week1/product/ProductController.java
+++ week1/src/main/java/com/example/week1/product/ProductController.java
@@ -1,10 +1,7 @@
 package com.example.week1.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
@@ -15,5 +12,11 @@ public class ProductController {
     @ResponseBody
     public Products searchProductByKeywords(@RequestParam String keyword) {
         return productService.searchProducts(keyword);
+    }
+
+    @GetMapping("/products/{id}")
+    @ResponseBody
+    public Product getProductDetailById(@PathVariable Integer id) {
+        return productService.getProduct(id);
     }
 }
