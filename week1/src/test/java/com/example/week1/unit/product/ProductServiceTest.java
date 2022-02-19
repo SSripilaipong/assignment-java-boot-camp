061a0e01be78d9a6401a578776023293ca4e243c
SSripilaipong <santhapon.s@siametrics.com>
2022-02-19 21:33:22 +0700

    test: add unit test shouldFindProductById in ProductServiceTest
    ProductService.getProduct() should find product in ProductRepository
    
    Refs: #3

--- week1/src/test/java/com/example/week1/unit/product/ProductServiceTest.java
+++ week1/src/test/java/com/example/week1/unit/product/ProductServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static com.example.week1.unit.product.ProductDummyFactory.*;
 import static com.example.week1.unit.product.ProductDummyFactory.getDummyProductList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -38,5 +40,16 @@ public class ProductServiceTest {
         productService.addNewProduct(newProduct);
 
         verify(productRepository).save(newProduct);
+    }
+
+    @Test
+    void shouldFindProductById() {
+        ProductService productService = new ProductService();
+        productService.setProductRepository(productRepository);
+        when(productRepository.findById(1234)).thenReturn(Optional.of(getDummyProduct()));
+
+        Product product = productService.getProduct(1234);
+
+        assertEquals(getDummyProduct(), product);
     }
 }
