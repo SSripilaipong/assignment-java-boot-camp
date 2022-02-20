1566187fd7e9a627d8d3b2757515877e6a8ee608
SSripilaipong <santhapon.s@siametrics.com>
2022-02-20 15:19:28 +0700

    feat: update CartController.addMyCartItem() for shouldReturnStatusCode201AfterAddedItemToCart in CartControllerTest
    POST /cart/items response with status code 201
    
    Refs: #4

--- week1/src/main/java/com/example/week1/cart/CartController.java
+++ week1/src/main/java/com/example/week1/cart/CartController.java
@@ -4,6 +4,7 @@ import com.example.week1.cart.request.CartItemAddingRequest;
 import com.example.week1.cart.response.CartItemAddedResponse;
 import com.example.week1.user.UserTokenManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +21,7 @@ public class CartController {
             value = "/cart/items",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
     public CartItemAddedResponse addMyCartItem(
             @RequestHeader("Authorization") String token, @RequestBody CartItemAddingRequest request) {
         cartService.addItemToMyCart(tokenManager.decodeTokenToUsername(token), request.toCartItem());
