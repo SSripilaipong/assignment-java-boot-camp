158c8b594aa3c5abe856a1b1df5398a5d6cd82ba
SSripilaipong <santhapon.s@siametrics.com>
2022-02-21 17:37:18 +0700

    feat: add initial default address for demo
    Refs: #7

--- week1/src/main/java/com/example/week1/Week1Application.java
+++ week1/src/main/java/com/example/week1/Week1Application.java
@@ -1,5 +1,7 @@
 package com.example.week1;
 
+import com.example.week1.delivery.address.Address;
+import com.example.week1.delivery.address.AddressService;
 import com.example.week1.sales.product.Product;
 import com.example.week1.sales.product.ProductService;
 import com.example.week1.user.UserService;
@@ -18,6 +20,9 @@ public class Week1Application {
 	@Autowired
 	private ProductService productService;
 
+	@Autowired
+	private AddressService addressService;
+
 	@PostConstruct
 	public void createInitialUser() {
 		userService.register("Santhapon", "Admin1234");
@@ -30,6 +35,14 @@ public class Week1Application {
 		productService.addNewProduct(new Product(2, "POCA SHOE NMD Sneakers Fashion", 399.00, "These are the best shoes.", "Poca Shoes", "Casual"));
 		productService.addNewProduct(new Product(3, "Something Irrelevant", 99999.0, "", "", ""));
 		productService.addNewProduct(new Product(4, "Adidas NMD R1 Color Core Black", 7990.00, "", "", ""));
+	}
+
+	@PostConstruct
+	public void setInitialDefaultAddress() {
+		Address address = new Address("Santhapon Sripilaipong", "Somewhere in Thailand",
+				"12345", "Somewhere", "Krung Thep Maha Nakhon (Bangkok)",
+				"0999999999");
+		addressService.setMyDefaultAddress("Santhapon", address);
 	}
 
 	public static void main(String[] args) {
