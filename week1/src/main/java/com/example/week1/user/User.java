9a01cc2c7f8b5a0faaa16754e268991d2cb9dea3
SSripilaipong <santhapon.s@siametrics.com>
2022-03-09 13:25:01 +0700

    refactor: use Lombok in user.User
    Refs: #1

--- week1/src/main/java/com/example/week1/user/User.java
+++ week1/src/main/java/com/example/week1/user/User.java
@@ -1,35 +1,22 @@
 package com.example.week1.user;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter @Setter @NoArgsConstructor
 public class User {
     @Id
     private String username;
     private String password;
-
-    public User() {
-    }
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
