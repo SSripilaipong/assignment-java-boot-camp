b6dcb1959ee9c6ea94133ddd3f850306d0ab7bb7
SSripilaipong <santhapon.s@siametrics.com>
2022-03-09 12:54:59 +0700

    refactor: use Lombok in sales.cart.request.SelectCartPaymentMethodRequest
    Refs: #11

--- week1/src/main/java/com/example/week1/sales/cart/request/SelectCartPaymentMethodRequest.java
+++ week1/src/main/java/com/example/week1/sales/cart/request/SelectCartPaymentMethodRequest.java
@@ -1,17 +1,18 @@
 package com.example.week1.sales.cart.request;
 
 import com.example.week1.rest.JsonConvertible;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Objects;
 
+@Getter @Setter @NoArgsConstructor
 public class SelectCartPaymentMethodRequest implements JsonConvertible {
 
     private Integer paymentMethodId;
-
-    public SelectCartPaymentMethodRequest() {
-    }
 
     public SelectCartPaymentMethodRequest(int paymentMethodId) {
         this.paymentMethodId = paymentMethodId;
@@ -37,11 +38,4 @@ public class SelectCartPaymentMethodRequest implements JsonConvertible {
         return Objects.equals(paymentMethodId, that.paymentMethodId);
     }
 
-    public Integer getPaymentMethodId() {
-        return paymentMethodId;
-    }
-
-    public void setPaymentMethodId(Integer paymentMethodId) {
-        this.paymentMethodId = paymentMethodId;
-    }
 }
