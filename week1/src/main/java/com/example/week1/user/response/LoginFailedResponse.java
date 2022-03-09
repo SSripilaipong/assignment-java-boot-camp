156658099a0668900915b97a9ea1a95b5f676fa6
SSripilaipong <santhapon.s@siametrics.com>
2022-03-09 13:22:52 +0700

    refactor: use Lombok in user.response.LoginFailedResponse
    Refs: #11

--- week1/src/main/java/com/example/week1/user/response/LoginFailedResponse.java
+++ week1/src/main/java/com/example/week1/user/response/LoginFailedResponse.java
@@ -1,16 +1,15 @@
 package com.example.week1.user.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
 public class LoginFailedResponse {
     private String message;
-
-    public LoginFailedResponse() {
-    }
 
     public LoginFailedResponse(String message) {
         this.message = message;
     }
 
-    public String getMessage() {
-        return this.message;
-    }
 }
