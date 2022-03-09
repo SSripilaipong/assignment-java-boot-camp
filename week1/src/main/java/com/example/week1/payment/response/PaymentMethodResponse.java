bfa1c47dbf078c25497d563e3277a8495677db8e
SSripilaipong <santhapon.s@siametrics.com>
2022-03-09 12:44:04 +0700

    refactor: use Lombok in payment.response.PaymentMethodResponse
    Refs: #11

--- week1/src/main/java/com/example/week1/payment/response/PaymentMethodResponse.java
+++ week1/src/main/java/com/example/week1/payment/response/PaymentMethodResponse.java
@@ -1,9 +1,13 @@
 package com.example.week1.payment.response;
 
 import com.example.week1.payment.PaymentMethod;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
+@Getter @Setter @NoArgsConstructor
 public class PaymentMethodResponse {
     private Integer id;
     private String method;
@@ -11,9 +15,6 @@ public class PaymentMethodResponse {
     private String cardNumber;
     private String expire;
     private String cvv;
-
-    public PaymentMethodResponse() {
-    }
 
     public PaymentMethodResponse(Integer id, String method, String ownerName, String cardNumber, String expire, String cvv) {
         this.id = id;
@@ -37,51 +38,4 @@ public class PaymentMethodResponse {
         return Objects.equals(id, that.id) && Objects.equals(method, that.method) && Objects.equals(ownerName, that.ownerName) && Objects.equals(cardNumber, that.cardNumber) && Objects.equals(expire, that.expire) && Objects.equals(cvv, that.cvv);
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    public void setMethod(String method) {
-        this.method = method;
-    }
-
-    public String getOwnerName() {
-        return ownerName;
-    }
-
-    public void setOwnerName(String ownerName) {
-        this.ownerName = ownerName;
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-    public String getExpire() {
-        return expire;
-    }
-
-    public void setExpire(String expire) {
-        this.expire = expire;
-    }
-
-    public String getCvv() {
-        return cvv;
-    }
-
-    public void setCvv(String cvv) {
-        this.cvv = cvv;
-    }
 }
