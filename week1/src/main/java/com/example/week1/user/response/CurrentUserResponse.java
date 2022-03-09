cdf86da2472a3fc6f99a01e59d994837ab32df21
SSripilaipong <santhapon.s@siametrics.com>
2022-03-09 13:21:15 +0700

    refactor: use Lombok in user.response.CurrentUserResponse
    Refs: #11

--- week1/src/main/java/com/example/week1/user/response/CurrentUserResponse.java
+++ week1/src/main/java/com/example/week1/user/response/CurrentUserResponse.java
@@ -1,20 +1,15 @@
 package com.example.week1.user.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
 public class CurrentUserResponse {
     private String username;
-
-    public CurrentUserResponse() {
-    }
 
     public CurrentUserResponse(String username) {
         this.username = username;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
