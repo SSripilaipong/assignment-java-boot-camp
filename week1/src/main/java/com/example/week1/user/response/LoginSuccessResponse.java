d1717ff5ed09d9d0f2fbef96b6ef8b17591decf6
SSripilaipong <santhapon.s@siametrics.com>
2022-03-09 13:23:53 +0700

    refactor: use Lombok in user.response.LoginSuccessResponse
    Refs: #11

--- week1/src/main/java/com/example/week1/user/response/LoginSuccessResponse.java
+++ week1/src/main/java/com/example/week1/user/response/LoginSuccessResponse.java
@@ -1,20 +1,15 @@
 package com.example.week1.user.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
 public class LoginSuccessResponse {
     private String token;
-
-    public LoginSuccessResponse() {
-    }
 
     public LoginSuccessResponse(String token) {
         this.token = token;
     }
 
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
