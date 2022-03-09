9d09c63f50211dd62a71b0a71f81097427735969
SSripilaipong <santhapon.s@siametrics.com>
2022-03-09 13:00:08 +0700

    refactor: use Lombok for sales.cart.response.CartItemAddedResponse
    Refs: #11

--- week1/src/main/java/com/example/week1/sales/cart/response/CartItemAddedResponse.java
+++ week1/src/main/java/com/example/week1/sales/cart/response/CartItemAddedResponse.java
@@ -1,20 +1,15 @@
 package com.example.week1.sales.cart.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
 public class CartItemAddedResponse {
     private String message;
-
-    public CartItemAddedResponse() {
-    }
 
     public CartItemAddedResponse(String message) {
         this.message = message;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
